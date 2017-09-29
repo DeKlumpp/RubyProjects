cc2896214fdfb8dfbf9c9acf7e0def2b75ca895e
Dennis Klumpp <klumppdennis@gmail.com>
2017-09-29 14:48:30 -0300

    gem sqli

--- IntegracaoRevendedor/api_client.rb
+++ IntegracaoRevendedor/api_client.rb
@@ -5,11 +5,11 @@ client = Faraday.new(url: 'http://localhost:3000') do |config|
 end
 
 response = client.post do |req|
-    #req.url '/api/usuario/login'
-    req.url '/api/moto/obterEstoqueMoto'
+    req.url '/api/usuario/login'
+    #req.url '/api/moto/obterEstoqueMoto'
     req.headers['Content-Type'] = 'application/json'
-    #req.body = '{ "usuario": { "cnpj": "03347828000109", "email":  "teste.integracao@webmotors.com.br", "senha": "teste123" } }'
-    req.body='{ "token": "7bd0a8430815f2928b7ef1e135a52496" }'
+    req.body = '{ "usuario": { "cnpj": "03347828000109", "email":  "teste.integracao@webmotors.com.br", "senha": "teste123" } }'
+    #req.body='{ "token": "7bd0a8430815f2928b7ef1e135a52496" }'
 end
 
 require 'oj'
